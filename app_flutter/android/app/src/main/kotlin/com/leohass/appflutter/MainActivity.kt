c706960351c51e28f6b10d115c2321815daad5b3
Leonid Hass <lionshostka@gmail.com>
2020-07-29 12:22:07 +0300

    Add files via upload

---
+++ app_flutter/android/app/src/main/kotlin/com/leohass/appflutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.leohass.appflutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
